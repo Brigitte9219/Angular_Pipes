1409f2a9495c3e795950c6c3580dddf8d5e2b193
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2023-12-06 16:30:51 -0500

    Pipes- Slice:1:5-En el arreglo del 1 al 10 me trae los numeros del 2 al 5..la posicion inicia en 0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre = 'Capitán América';
+  nombre = 'Brigitte';
+
+  arreglo=[1,2,3,4,5,6,7,8,9,10];
 }
