77156bb377851e2a1b2636b6f31ced4d54cdf2f1
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2023-12-06 16:43:14 -0500

    Pipes- number- que es para los decimales..si quieres ver parte entera y decimales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre = 'Brigitte';
+  nombre:string = 'Brigitte';
 
   arreglo=[1,2,3,4,5,6,7,8,9,10];
+
+  PI:number= Math.PI;
 }
