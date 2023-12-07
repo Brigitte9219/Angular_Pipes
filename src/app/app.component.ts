892b8815a6632034b0b1f49517ab3bf1d04b2f8a
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2023-12-07 08:33:34 -0500

    Pipes-async-Se espera que el objeto asincrono responda en cierto tiempo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent {
   PI:number= Math.PI;
   porcentaje: number= 0.234;
   salario: number= 1234.5;
+
+//Trabajar con algo asincrono
+  valorPromesa = new Promise<string>((resolve)=>{
+
+    setTimeout(()=>{
+      resolve('llego la data');
+    }, 4500);
+  });
+
+
   heroe={
     nombre:'Logan',
     clave:'Wolverine',
